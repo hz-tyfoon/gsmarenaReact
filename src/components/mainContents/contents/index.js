f17a6f8ed814bd48e0279e30ea3fa024f98a9074
hz-tyfoon <md.tifun@gmail.com>
2021-03-30 23:10:40 +0600

    markup done, now implementing react's power left

--- src/components/mainContents/contents/index.js
+++ src/components/mainContents/contents/index.js
@@ -1,80 +1,12 @@
-import Banner from "../../../banner.png";
-import { Link } from "react-router-dom";
+import Signledetails from "./signleDetails";
+import TopPhones from "./topPhones";
 
 export default function Content({ className }) {
   return (
     <div className={className}>
-      <div className="banner">
-        <img src={Banner} alt="Banner Img" />
-        <div className="jhapsha d_flx">
-          <h2 className="title ">Top Phones</h2>
-        </div>
-      </div>
+      {/* <Signledetails /> */}
 
-      <div className="list_phone jc_spb d_flx flw_w">
-        <div className="device ta_c">
-          <Link to="/">
-            <img src="images/single_phone.png" alt="" />
-            <h2 className="">Xiaomi Mi 11 Lite</h2>
-          </Link>
-        </div>
-
-        <div className="device ta_c">
-          <Link to="/">
-            <img src="images/single_phone.png" alt="" />
-            <h2 className="">Xiaomi Mi 11 Lite</h2>
-          </Link>
-        </div>
-
-        <div className="device ta_c">
-          <Link to="/">
-            <img src="images/single_phone.png" alt="" />
-            <h2 className="">Xiaomi Mi 11 Lite</h2>
-          </Link>
-        </div>
-
-        <div className="device ta_c">
-          <Link to="/">
-            <img src="images/single_phone.png" alt="" />
-            <h2 className="">Xiaomi Mi 11 Lite</h2>
-          </Link>
-        </div>
-
-        <div className="device ta_c">
-          <Link to="/">
-            <img src="images/single_phone.png" alt="" />
-            <h2 className="">Xiaomi Mi 11 Lite</h2>
-          </Link>
-        </div>
-
-        <div className="device ta_c">
-          <Link to="/">
-            <img src="images/single_phone.png" alt="" />
-            <h2 className="">Xiaomi Mi 11 Lite</h2>
-          </Link>
-        </div>
-
-        <div className="device ta_c">
-          <Link to="/">
-            <img src="images/single_phone.png" alt="" />
-            <h2 className="">Xiaomi Mi 11 Lite</h2>
-          </Link>
-        </div>
-
-        <div className="device ta_c">
-          <Link to="/">
-            <img src="images/single_phone.png" alt="" />
-            <h2 className="">Xiaomi Mi 11 Lite</h2>
-          </Link>
-        </div>
-
-        <div className="device ta_c">
-          <Link to="/">
-            <img src="images/single_phone.png" alt="" />
-            <h2 className="">Xiaomi Mi 11 Lite</h2>
-          </Link>
-        </div>
-      </div>
+      <TopPhones />
     </div>
   );
 }
---
+++ src/components/mainContents/contents/topPhones.js
@@ -0,0 +1,80 @@
+import Banner from "../../../banner.png";
+import { Link } from "react-router-dom";
+
+export default function TopPhones() {
+  return (
+    <>
+      <div className="banner">
+        <img src={Banner} alt="Banner Img" />
+        <div className="jhapsha d_flx">
+          <h2 className="title ">Top Phones</h2>
+        </div>
+      </div>
+
+      <div className="list_phone jc_spb d_flx flw_w">
+        <div className="device ta_c">
+          <Link to="/">
+            <img src="images/single_phone.png" alt="" />
+            <h2 className="">Xiaomi Mi 11 Lite</h2>
+          </Link>
+        </div>
+
+        <div className="device ta_c">
+          <Link to="/">
+            <img src="images/single_phone.png" alt="" />
+            <h2 className="">Xiaomi Mi 11 Lite</h2>
+          </Link>
+        </div>
+
+        <div className="device ta_c">
+          <Link to="/">
+            <img src="images/single_phone.png" alt="" />
+            <h2 className="">Xiaomi Mi 11 Lite</h2>
+          </Link>
+        </div>
+
+        <div className="device ta_c">
+          <Link to="/">
+            <img src="images/single_phone.png" alt="" />
+            <h2 className="">Xiaomi Mi 11 Lite</h2>
+          </Link>
+        </div>
+
+        <div className="device ta_c">
+          <Link to="/">
+            <img src="images/single_phone.png" alt="" />
+            <h2 className="">Xiaomi Mi 11 Lite</h2>
+          </Link>
+        </div>
+
+        <div className="device ta_c">
+          <Link to="/">
+            <img src="images/single_phone.png" alt="" />
+            <h2 className="">Xiaomi Mi 11 Lite</h2>
+          </Link>
+        </div>
+
+        <div className="device ta_c">
+          <Link to="/">
+            <img src="images/single_phone.png" alt="" />
+            <h2 className="">Xiaomi Mi 11 Lite</h2>
+          </Link>
+        </div>
+
+        <div className="device ta_c">
+          <Link to="/">
+            <img src="images/single_phone.png" alt="" />
+            <h2 className="">Xiaomi Mi 11 Lite</h2>
+          </Link>
+        </div>
+
+        <div className="device ta_c">
+          <Link to="/">
+            <img src="images/single_phone.png" alt="" />
+            <h2 className="">Xiaomi Mi 11 Lite</h2>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
